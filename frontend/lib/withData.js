952e24add4ac643d70643183a1d885d70b29e943
Kevin Pena <kevinpena160@gmail.com>
2019-03-13 16:11:25 -0500

    updating

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,10 +1,9 @@
 import withApollo from 'next-with-apollo';
 import ApolloClient from 'apollo-boost';
-// import { endpoint } from '../config';
+import { endpoint } from '../config';
 // import { LOCAL_STATE_QUERY } from '../components/Cart';
 
 function createClient({ headers }) {
-  const endpoint = 'http://localhost:4000';
   return new ApolloClient({
     uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
     request: operation => {
